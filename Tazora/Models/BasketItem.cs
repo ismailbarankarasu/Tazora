88a1f95816e0b4ca5d4791fb520521cbeef2da3c
İsmail Baran <ismailbaran04@gmail.com>
2026-08-23 23:57:04 +0300

    Add basket item model

---
+++ Tazora/Models/BasketItem.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace Tazora.Models;
+
+[Table("BasketItems")]
+public class BasketItem
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [Indexed, Unique, NotNull]
+    public int ProductId { get; set; }
+
+    [NotNull]
+    public int Quantity { get; set; } = 1;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
